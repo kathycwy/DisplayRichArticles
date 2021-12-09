59ce36062f986ff9d1049211913b73b66c14ba73
Samuel <samvictor232@gmail.com>
2021-12-09 16:45:45 +0100

    Rearrange storyboard flow for UILabel & UITextView

---
+++ DisplayRichArticles/UILabel&UITextView/commentSectionViewController.swift
@@ -0,0 +1,15 @@
+//
+//  commentSectionViewController.swift
+//  DisplayRichArticles
+//
+//  Created by Samuel Kwong on 09.12.21.
+//
+
+import UIKit
+
+class commentSectionViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
