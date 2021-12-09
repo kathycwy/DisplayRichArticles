1b363bbcf3e1e7ff2609f3c9474e7ce719bf293c
Wingyin Chan <wing.yin.chan@rwth-aachen.de>
2021-12-09 15:29:05 +0800

    updated attr text for UILabel & UITextView

--- DisplayRichArticles/UITextViewController.swift
+++ DisplayRichArticles/UITextViewController.swift
@@ -21,10 +21,10 @@ class UITextViewController: UIViewController {
             
         view.addSubview(scrollView)
         
-        self.setUITextView()
+        self.setView()
     }
     
-    func setUITextView() {
+    func setView() {
         
         let itemX = 40
         var itemY = 40
@@ -34,101 +34,90 @@ class UITextViewController: UIViewController {
         let labelTextViewOffset = 25
         let itemY_increment = 160
         
-        //Default UITextView
+        
+        //////////////////////////////////////////////////////////  Basic  //////////////////////////////////////////////////////////
+        // UILabel
         let uiLabel_default = UILabel(frame: CGRect(x: itemX, y: itemY, width: itemWidth, height: labelHeight))
-        uiLabel_default.text = "Default"
+        uiLabel_default.text = "Basic UILabel"
+        
+        // UITextView
         let uiText_default = UITextView()
         uiText_default.frame = CGRect(x: itemX, y: itemY+labelTextViewOffset, width: itemWidth, height: itemHeight)
         uiText_default.text = """
         UITextView supports the display of text using custom style information and also supports text editing. You typically use a text view to display multiple lines of text, such as when displaying the body of a large text document.
         This class supports multiple text styles through use of the attributedText property. (Styled text is not supported in versions of iOS earlier than iOS 6.) Setting a value for this property causes the text view to use the style information provided in the attributed string. You can still use the font, textColor, and textAlignment properties to set style attributes, but those properties apply to all of the text in the text view. It’s recommended that you use a text view—and not a UIWebView object—to display both plain and rich text in your app.
         """
+        
         itemY += itemY_increment
         scrollView.addSubview(uiLabel_default)
         scrollView.addSubview(uiText_default)
         
+
         
-        //UITextView with styled frame
-        let uiLabel_styledFrame = UILabel(frame: CGRect(x: itemX, y: itemY, width: itemWidth, height: labelHeight))
-        uiLabel_styledFrame.text = "Styled Frame"
-        let uiText_styledFrame = UITextView()
-        uiText_styledFrame.frame = CGRect(x: itemX, y: itemY+labelTextViewOffset, width: itemWidth, height: itemHeight)
-        uiText_styledFrame.layer.borderColor = UIColor.black.cgColor
-        uiText_styledFrame.layer.borderWidth = 1.0
-        uiText_styledFrame.layer.cornerRadius = 8
-        uiText_styledFrame.backgroundColor = UIColor.cyan
-        uiText_styledFrame.text = """
+        
+        //////////////////////////////////////////////////////////  Custom Styles  //////////////////////////////////////////////////////////
+        // UILabel
+        let uiLabel_customStyles = UILabel(frame: CGRect(x: itemX, y: itemY, width: itemWidth, height: labelHeight))
+        uiLabel_customStyles.textColor = UIColor.red
+        uiLabel_customStyles.font = UIFont(name: "Bradley Hand Bold", size: 20)
+        uiLabel_customStyles.text = "UILabel with custom styles"
+        
+        // UITextView
+        let uiText_customStyles = UITextView()
+        uiText_customStyles.frame = CGRect(x: itemX, y: itemY+labelTextViewOffset, width: itemWidth, height: itemHeight)
+        uiText_customStyles.layer.borderColor = UIColor.black.cgColor
+        uiText_customStyles.layer.borderWidth = 1.0
+        uiText_customStyles.layer.cornerRadius = 8
+        uiText_customStyles.backgroundColor = UIColor.lightGray
+        uiText_customStyles.textColor = UIColor.blue
+        uiText_customStyles.font = UIFont(name: "Courier", size: 20)
+        uiText_customStyles.textAlignment = NSTextAlignment.center
+        uiText_customStyles.text = """
         UITextView supports the display of text using custom style information and also supports text editing. You typically use a text view to display multiple lines of text, such as when displaying the body of a large text document.
         This class supports multiple text styles through use of the attributedText property. (Styled text is not supported in versions of iOS earlier than iOS 6.) Setting a value for this property causes the text view to use the style information provided in the attributed string. You can still use the font, textColor, and textAlignment properties to set style attributes, but those properties apply to all of the text in the text view. It’s recommended that you use a text view—and not a UIWebView object—to display both plain and rich text in your app.
         """
+        
         itemY += itemY_increment
-        scrollView.addSubview(uiLabel_styledFrame)
-        scrollView.addSubview(uiText_styledFrame)
+        scrollView.addSubview(uiLabel_customStyles)
+        scrollView.addSubview(uiText_customStyles)
+
+
+
+
+        //////////////////////////////////////////////////////////  Attributed Text  //////////////////////////////////////////////////////////
+        // UILabel
+        let uiLabelHtmlString = "<b>UILabel</b> with <u><i>html</i> elements</u>"
+        let uiLabelData = uiLabelHtmlString.data(using: .utf8)!
+        let uiLabelAttributedString = try? NSAttributedString(
+            data: uiLabelData,
+            options: [.documentType: NSAttributedString.DocumentType.html],
+            documentAttributes: nil)
         
-        
-        //UITextView with styled text
-        let uiLabel_styledText = UILabel(frame: CGRect(x: itemX, y: itemY, width: itemWidth, height: labelHeight))
-        uiLabel_styledText.text = "Styled Text"
-        let uiText_styledText = UITextView()
-        uiText_styledText.frame = CGRect(x: itemX, y: itemY+labelTextViewOffset, width: itemWidth, height: itemHeight)
-        uiText_styledText.layer.borderColor = UIColor.lightGray.cgColor
-        uiText_styledText.layer.borderWidth = 1.0
-        uiText_styledText.layer.cornerRadius = 8
-        uiText_styledText.textColor = UIColor.blue
-        uiText_styledText.font = UIFont(name: "Courier", size: 20)
-        uiText_styledText.textAlignment = NSTextAlignment.center
-        uiText_styledText.text = """
-        UITextView supports the display of text using custom style information and also supports text editing. You typically use a text view to display multiple lines of text, such as when displaying the body of a large text document.
-        This class supports multiple text styles through use of the attributedText property. (Styled text is not supported in versions of iOS earlier than iOS 6.) Setting a value for this property causes the text view to use the style information provided in the attributed string. You can still use the font, textColor, and textAlignment properties to set style attributes, but those properties apply to all of the text in the text view. It’s recommended that you use a text view—and not a UIWebView object—to display both plain and rich text in your app.
-        """
-        itemY += itemY_increment
-        scrollView.addSubview(uiLabel_styledText)
-        scrollView.addSubview(uiText_styledText)
-        
-        
-        //UITextView with disabled behavior
-        let uiLabel_disabledBehavior = UILabel(frame: CGRect(x: itemX, y: itemY, width: itemWidth, height: labelHeight))
-        uiLabel_disabledBehavior.text = "Disabled Behavior"
-        let uiText_disabledBehavior = UITextView()
-        uiText_disabledBehavior.frame = CGRect(x: itemX, y: itemY+labelTextViewOffset, width: itemWidth, height: itemHeight)
-        uiText_disabledBehavior.layer.borderColor = UIColor.lightGray.cgColor
-        uiText_disabledBehavior.layer.borderWidth = 1.0
-        uiText_disabledBehavior.layer.cornerRadius = 8
-        uiText_disabledBehavior.isEditable = false
-        uiText_disabledBehavior.isSelectable = false
-        uiText_disabledBehavior.isScrollEnabled = false
-        uiText_disabledBehavior.text = """
-        UITextView supports the display of text using custom style information and also supports text editing. You typically use a text view to display multiple lines of text, such as when displaying the body of a large text document.
-        This class supports multiple text styles through use of the attributedText property. (Styled text is not supported in versions of iOS earlier than iOS 6.) Setting a value for this property causes the text view to use the style information provided in the attributed string. You can still use the font, textColor, and textAlignment properties to set style attributes, but those properties apply to all of the text in the text view. It’s recommended that you use a text view—and not a UIWebView object—to display both plain and rich text in your app.
-        """
-        itemY += itemY_increment
-        scrollView.addSubview(uiLabel_disabledBehavior)
-        scrollView.addSubview(uiText_disabledBehavior)
-        
-        
-        //UITextView with attributed text
         let uiLabel_attributedText = UILabel(frame: CGRect(x: itemX, y: itemY, width: itemWidth, height: labelHeight))
-        uiLabel_attributedText.text = "Attributed Text"
+        uiLabel_attributedText.attributedText = uiLabelAttributedString
         
-        let text = """
-        UITextView supports the display of text using custom style information and also supports text editing. You typically use a text view to display multiple lines of text, such as when displaying the body of a large text document.
-        This class supports multiple text styles through use of the attributedText property. (Styled text is not supported in versions of iOS earlier than iOS 6.) Setting a value for this property causes the text view to use the style information provided in the attributed string. You can still use the font, textColor, and textAlignment properties to set style attributes, but those properties apply to all of the text in the text view. It’s recommended that you use a text view—and not a UIWebView object—to display both plain and rich text in your app.
+        // UITextView
+        let uiTextHtmlString = """
+        <h3>UITextView supports the display of text using custom style information and also supports text editing.</h3> You typically use a text view to <u>display multiple lines of text</u>, such as when <i>displaying the body of a large text document</i>.
+        This class supports multiple text styles through use of the <b>attributedText</b> property. <del>(Styled text is not supported in versions of iOS earlier than iOS 6.)</del> Setting a value for this property causes the text view to use the style information provided in the attributed string. You can still use the font, textColor, and textAlignment properties to set style attributes, but those properties apply to all of the text in the text view. It’s recommended that you use a text view—and not a UIWebView object—to display both plain and rich text in your app.
         """
-        let attributes: [NSAttributedString.Key: Any] = [
-            .font: UIFont(name: "Bradley Hand Bold", size: 15)!,
-            .foregroundColor: UIColor.orange
-        ]
-        let attributedText = NSMutableAttributedString(string: text, attributes: attributes)
+        let uiTextData = uiTextHtmlString.data(using: .utf8)!
+        let uiTextAttributedString = try? NSAttributedString(
+            data: uiTextData,
+            options: [.documentType: NSAttributedString.DocumentType.html],
+            documentAttributes: nil)
         
         let uiText_attributedText = UITextView()
         uiText_attributedText.frame = CGRect(x: itemX, y: itemY+labelTextViewOffset, width: itemWidth, height: itemHeight)
         uiText_attributedText.layer.borderColor = UIColor.lightGray.cgColor
         uiText_attributedText.layer.borderWidth = 1.0
         uiText_attributedText.layer.cornerRadius = 8
-        uiText_attributedText.attributedText = attributedText
+        uiText_attributedText.attributedText = uiTextAttributedString
+        
         itemY += itemY_increment
         scrollView.addSubview(uiLabel_attributedText)
         scrollView.addSubview(uiText_attributedText)
+        
     }
-    
+
 }
