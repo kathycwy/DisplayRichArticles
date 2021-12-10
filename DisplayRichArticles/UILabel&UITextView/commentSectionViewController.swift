d64fbe196a0cd0da31e658c5bd31b7db093dc1cc
Samuel <samvictor232@gmail.com>
2021-12-10 12:22:30 +0100

    Implemented comment section feature, Comments and UITextField not yet autoresizable. Not sure how to incorporate HTML?

--- DisplayRichArticles/UILabel&UITextView/commentSectionViewController.swift
+++ DisplayRichArticles/UILabel&UITextView/commentSectionViewController.swift
@@ -9,7 +9,40 @@ import UIKit
 
 class commentSectionViewController: UIViewController {
     
+    @IBOutlet weak var txtComment: UITextView!
+    @IBOutlet weak var tblCommentView: UITableView!
+    var arrComments = [String]()
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        txtComment.becomeFirstResponder()
+    }
+    
+    @IBAction func btnSendTapped(_ sender: UIButton) {
+        let commentTxt = txtComment.text!
+        arrComments.append(commentTxt)
+        tblCommentView.reloadData()
+        txtComment.resignFirstResponder()
+    }
+    
+}
+
+extension commentSectionViewController: UITableViewDataSource{
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        arrComments.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = arrComments[indexPath.row]
+        return cell
     }
 }
